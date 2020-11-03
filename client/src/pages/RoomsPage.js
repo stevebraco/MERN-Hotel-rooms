69dd33995144318252c969704cc80b9d8714c4da
anthowelc <welc.anthony@gmail.com>
2020-11-03 18:06:12 +0100

    first commit

---
+++ client/src/pages/RoomsPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Rooms from '../components/Rooms'
+import Room from '../components/Room'
+
+const RoomsPage = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Rooms />} />
+      <Route path=':id' element={<Room />} />
+    </Routes>
+  )
+}
+
+export default RoomsPage
